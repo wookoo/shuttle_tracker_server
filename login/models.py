aa64f1a254dd97ee85a255b302bfbb6a5fabbb70
wookoo <nanayagoon@daum.net>
2021-09-28 22:29:45 +0900

    모델명 변수 수정

--- login/models.py
+++ login/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Account(models.Model):
     id = models.CharField(max_length=20,primary_key=True)
-    pw = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
     company = models.CharField(max_length=6)
     phone = models.CharField(max_length=11)
     name = models.CharField(max_length=5)

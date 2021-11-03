b8ca932ccbaa4829e7ced9241d07b4e412b1a5ba
wookoo <nanayagoon@daum.net>
2021-11-03 14:11:33 +0900

    CORS 오류 해결

--- shuttle_tracker_server/settings.py
+++ shuttle_tracker_server/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
